72f690e5b870a60af7103bf00909fdef08e3dcbf
Enzo <enzo.rp.90@gmail.com>
2017-09-11 17:06:44 -0500

    added configuration to S3

--- vigoty/settings/prod.py
+++ vigoty/settings/prod.py
@@ -11,13 +11,26 @@ INSTALLED_APPS += PRODUCTION_APPS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+# AMAZON S3
+AWS_STORAGE_BUCKET_NAME = 'vigoty-do'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'vigoty.s3.StaticStorage'
+STATIC_URL = 'https://'+AWS_STORAGE_BUCKET_NAME+'.s3.amazonaws.com/'+STATICFILES_LOCATION+'/'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'vigoty.s3.MediaStorage'
+MEDIA_URL = 'https://'+AWS_STORAGE_BUCKET_NAME+'.s3.amazonaws.com/'+MEDIAFILES_LOCATION+'/'
+
+AWS_QUERYSTRING_AUTH = False
 
 django.setup()
 LOGIN_URL = reverse('login')
